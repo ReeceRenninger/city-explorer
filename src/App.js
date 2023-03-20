38cde07e55e2a3ab6413f8000c1a4edc189352ff
ReeceRenninger <renningerreece@gmail.com>
2023-03-20 12:26:24 -0700

    working page with error message displaying

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ class App extends React.Component {
   getCityData = async (event) => {
     event.preventDefault();
 
+    // try this 
     try {
-      // try this 
       let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
 
       let cityDataFromAxios = await axios.get(url);
@@ -34,27 +34,40 @@ class App extends React.Component {
         cityData: cityDataFromAxios.data[0],
         error: false
       });
+      // the try couldnt happen so this happens
+    } catch (error) {
+      console.log(error)
+      //TODO: set state with the error boolean and the error message
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      });
     }
 
-    } catch(error) {
-    console.log(error)
-    //TODO: set state with the error boolean and the error message
-    this.setState({
-      error: true,
-      errorMessage: error.message
-    })
-    // the try couldnt happen so this happens
   }
+  render() {
+    return (
+      <>
+        <h1>API Calls Baby!</h1>
 
-  // // TODO: USE AXIOS TO GET DATA FROM LOCATIONIQ - USING CITY IN STATE
-  // let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+        <form onSubmit={this.getCityData}>
+          <label>
+            <input type="text" onChange={this.handleCityInput} />
+          </label>
+          <button type="submit">Explore!</button>
+        </form>
 
-  // let cityDataFromAxios = await axios.get(url);
+        {/* TERNARY - WTF  */}
+        {
+          this.state.error
+            ? <p>{this.state.errorMessage}</p>
+            : <p>{this.state.cityData.display_name}</p>
+        }
+      
 
-  // // TODO: SET STATE WITH THE DAT THAT COMES BACK FROM 
-  // this.setState({
-  //   cityData: cityDataFromAxios.data[0]
-  // })
+      </>
+    )
+  }
 }
 // ** async/await - handle our asynchronous code
 // ** try/catch - handle our errors - TRY resolve our successful promises & CATCh handle rejected promises
@@ -62,30 +75,7 @@ class App extends React.Component {
 // !! https://maps.locationiq.com/v3/staticmap?key=pk.c99fe602101eebe5bbeb4261bd09aa7d&center=47.6038321,-122.330062&zoom=13
 
 
-render() {
-  return (
-    <>
-      <h1>API Calls Baby!</h1>
 
-      <form onSubmit={this.getCityData}>
-        <label>
-          <input type="text" onChange={this.handleCityInput} />
-        </label>
-        <button type="submit">Explore!</button>
-      </form>
-
-      {/* TERNARY - WTF  */}
-      {
-        this.state.error
-          ? <p>{this.sate.errorMessage}</p>
-          : <p>{this.state.cityData.display_name}</p>
-      }
-      <p>{this.state.cityData.display_name}</p>
-
-    </>
-  )
-}
-}
 
 
 
