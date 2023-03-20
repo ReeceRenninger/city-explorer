1b9e4fe511404d45e82e7c3da59b3830a76b223b
ReeceRenninger <renningerreece@gmail.com>
2023-03-20 10:55:55 -0700

    getting files sorted out from react creation and started axios setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      locationData: [],
+    }
+  }
+
+// ** async/await - handle our asynchronous code
+
+// ** GET LOCATION DATA
+
+handleGetLocationData = async (event) => {
+  event.preventDefault();
+
+  // TODO: USE AXIOS TO MAKE A CALL OUT TO LOCATION API
+  let locationData = await axios.get()
+  console.log(locationData);
+  // TODO: SET THAT DATA INTO STATE
+}
+
+  render() {
+    return (
+      <>
+        <h1>API Calls Baby!</h1>
+
+        <form>
+          <button type="submit">Submit Your City</button>
+        </form>
+      </>
+    )
+  }
 }
 
 export default App;
