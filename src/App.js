50170efdcf89a58313260e7c6832533a9404d201
ReeceRenninger <renningerreece@gmail.com>
2023-03-22 10:52:26 -0700

    refactor and prep for receiving weather api

--- src/App.js
+++ src/App.js
@@ -29,18 +29,46 @@ class App extends React.Component {
 
     })
   }
- 
-  handleSubmit = async (event) => {
+  //!! main grabber of API location IQ grabs information and passes it to other APIs 
+  getCityData = async (event) => {
     event.preventDefault();
 
     try {
-      this.getCityData();
-      let url = `${process.env.REACT_APP_SERVER}/weather?city=${this.state.city}`
-      let weatherData = await axios.get(url);
-      console.log(weatherData.data);
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+      
+      let cityDataFromAxios = await axios.get(url);
+      
+      this.setState({
+        cityData: cityDataFromAxios.data[0], // grabs the first instance of Seattle in data 
+        error: false
+      });
+
+      //TODO: WEATHER HANDLER 
+      let lat = cityDataFromAxios.data[0].lat;
+      let lon = cityDataFromAxios.data[0].lon;
+      this.handleWeather(lat, lon);
+
+    } catch (error) {
+      console.log('GetCityData' + error.message)
+      
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      });
+    }
+    
+  }
+  
+  handleWeather = async (lat, lon) => {
+    
+    try {
+      
+      let url = `${process.env.REACT_APP_SERVER}/weather?&searchQuery=${this.state.city}&lat=${lat}&lon=${lon}`
+      let weatherDataFromAxios = await axios.get(url);
+      console.log(weatherDataFromAxios.data);
 
       this.setState({
-        cityWeather: weatherData.data
+        cityWeather: weatherDataFromAxios.data
       });
       
     } catch (error) {
@@ -52,35 +80,13 @@ class App extends React.Component {
     }
   }
   
-  getCityData = async (event) => {
-    
-    try {
-      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
-      
-      let cityDataFromAxios = await axios.get(url);
-      
-      this.setState({
-        cityData: cityDataFromAxios.data[0],
-        error: false
-      });
-
-    } catch (error) {
-      console.log('GetCityData' + error.message)
-      //TODO: set state with the error boolean and the error message
-      this.setState({
-        error: true,
-        errorMessage: error.message
-      });
-    }
-    
-  }
 
   render() {
     return (
       <>
         <h1>API Calls!</h1>
         
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={this.getCityData}>
           <Form.Label>City Explorer</Form.Label>
             <Form.Control type="text" placeholder="Enter a city name here" onChange={this.handleCityInput} />
           <Button variant="info" type="submit">Explore!</Button>
@@ -94,6 +100,7 @@ class App extends React.Component {
             <ul>
               <p>{this.state.cityData.display_name}</p>
               <Image src= {`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=13`} alt='Map of selected location'/>
+
               <LocationCords 
               cityData={this.state.cityData}
               
